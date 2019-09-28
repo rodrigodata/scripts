ea862e8bf54a8468b49533b0c1c2d42bb45e20df
Rodrigo Andrade <warodrigoandrade@gmail.com>
2019-09-27 21:25:13 -0300

    Finalizado primeira versao de script conversor de CSV para Json.

--- csv/csv_to_json.sh
+++ csv/csv_to_json.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
-file="example.csv"
-total_number_lines=$(wc -l < $file)
+file_input=$1
+file_output=$2
+is_double_quote=$3
+
+total_number_lines=$(wc -l < $file_input)
 json_file=""
+# quote string (double ou single)
+q=false
 
 echo "Total de linhas no arquivo: $total_number_lines"
+echo "Caminho do arquivo informado: $1"
+
+# gerar json com double ou single quotes
+if [ "$is_double_quote" = false ]
+then 
+    q="'"
+else 
+    q="\""
+fi 
 
 # enquanto tivermos linhas para ler dentro do arquivo csv, iteramos sobre ele
 # a variavel $line se refere a linha de iteracao atual
@@ -22,20 +36,32 @@ do
 
     else
 
+        # abertura header json
+        header_json=""    
+
+        # caso seja a linha apos imediatamente a primeira, ou seja, apos a linha do header
+        # identificamos que seja a primeira linha com dados do csv, com isso, abrimos nosso json
+        # corretamente
+        if [ $i -eq 2 ]
+        then
+            json_file+="["
+        fi
+
         #echo "Processando linha -> $i -> $line"
         IFS=',' read -ra row_array <<< "$line"
 
         current_cell_index=0
         row_array_size="${#row_array[@]}"
-        json=""
+        
 
         # iteramos sobre todos os valores dentro do array $row_array
         for current_cell_value in "${row_array[@]}"
         do
+            
 
             current_header_value=${header_array[$current_cell_index]}
 
-            json+="'$current_header_value':'$current_cell_value'," 
+            header_json+="$q$current_header_value$q:$q$current_cell_value$q," 
             
             # acrescimo de contagem do index
             current_cell_index=$(( current_cell_index + 1 ))
@@ -43,7 +69,7 @@ do
             if [ $row_array_size -eq $current_cell_index ]
             then
 
-                IFS=',' read -ra json_array <<< "$json"
+                IFS=',' read -ra json_array <<< "$header_json"
                 template_json=""
                 template_json+="{"
                 
@@ -51,9 +77,21 @@ do
 
                 for current_json_value in "${json_array[@]}"                
                 do 
+                
                     if [ $current_json_index -eq "${#json_array[@]}" ]
                     then 
-                        template_json+="$current_json_value}"    
+
+                        template_json+="$current_json_value}"
+
+                        # caso o numero total de linhas seja igual ao index atual de iteracao
+                        # identificamos que este e o ultimo item a ser lido do csv. Com isso,
+                        # conseguimos fechar o json corretamente
+                        if [ $i -eq $((total_number_lines)) ]
+                        then 
+                            template_json+="]"              
+                        else 
+                            template_json+=","              
+                        fi 
 
                     else
                         current_json_index=$(( current_json_index + 1 ))
@@ -62,11 +100,7 @@ do
                     
                 done
 
-                # TODO: ajustar problema ao inserir ultimo objeto dentro de array com virgula
-                # TODO: separar melhorar logica de geracao de JSON dinamico
-
-                json_file+="$template_json,"                
-                
+                json_file+="$template_json"                
                 
             fi
             
@@ -74,7 +108,7 @@ do
 
     fi
 
-done < "$file"
+done < "$file_input"
 
-# output esperado -> {'id':'1','game':'destiny'},{'id':'2','game':'the surge 2'},{'id':'3','game':'lol'},
-echo $json_file
+# output esperado -> [{'id':'1','game':'destiny'},{'id':'2','game':'the surge 2'},{'id':'3','game':'lol'}]
+echo $json_file > $file_output
---
+++ csv/readme.md
@@ -0,0 +1,16 @@
+# CSV_TO_JSON.SH
+
+## Uso
+```shell
+chmod +x ./csv_to_json.sh
+```
+
+```shell
+./csv_to_json.sh ~/input.csv ~/output.json false
+```
+
+input de arquivo CSV -> `~/input.csv`
+
+output de arquivo JSON -> `~/output.json`
+
+double quote ou single quote(default == double quote) -> `false`
